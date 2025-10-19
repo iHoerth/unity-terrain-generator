3d25dedee45b34dc08847d41c4437b08abf26ab3
Ignacio Hoerth <hoerth.ignacio@gmail.com>
2025-10-19 16:56:41 -0300

    feat(TerrainGenerator): Perlin Noise implementation using FastNoise lib. Variables freq and seed included for code readability and dev testing

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -4,13 +4,29 @@ public class TerrainGenerator : MonoBehaviour
 {
 
     public GameObject cube;
+    public float scale = 0.05f; //controls noise frequency
+    public float freq = 0.05f;
+    public int seed = 42;
+
+    public float height = 50f; //controls noise maximum height
+    public int xwidth = 50;
+    public int zwidth = 50;
 
     void Start()
     {
-        for(int x = -10; x <= 10; x++)
-            for(int z = -10; z <= 10; z++)
+        FastNoise noise = new FastNoise();
+        
+        noise.SetNoiseType(FastNoise.NoiseType.Perlin);
+        noise.SetFrequency(freq);
+        noise.SetSeed(seed);
+        
+        for(int x = -xwidth; x <= xwidth; x++)
+            for(int z = -zwidth; z <= zwidth; z++)
             {
-                Instantiate(cube , new Vector3(x, 0 , z), Quaternion.identity);
+                float y = noise.GetNoise(x, z, 0) * height;
+                y = Mathf.Round(y); // discrete values like mc
+
+                Instantiate(cube , new Vector3(x, y , z), Quaternion.identity);
             }
     }
 
