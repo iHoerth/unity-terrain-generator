239976a4e046213e6c488661cfcaf25e8e57cf86
Ignacio Hoerth <hoerth.ignacio@gmail.com>
2025-11-06 00:59:48 -0300

    Code refactor in TerrainChunk, implementing neighbour chunk system to not draw borders if nb block is not air

--- Assets/Scripts/Terrain/TerrainChunk.cs
+++ Assets/Scripts/Terrain/TerrainChunk.cs
@@ -7,211 +7,180 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class TerrainChunk : MonoBehaviour
 {
+
+    
     // World variables
-    public int chunkHeight = 64;
-    public int chunkWidth = 64;
-    [SerializeField] int size = 10; // número de vértices por lado
+    public const int chunkHeight = 64;
+    public const int chunkWidth = 64;
+
+    public Vector2Int chunkCoord;
+    public WorldGenerator world;
 
     // Noise variables
     public float noiseScale = 0.8f;
     public float noiseAmplitude = 10f;
     public float freq = 0.05f;
     public int seed = 42;
+    FastNoise noise = new FastNoise();
 
-    void Start()
+    List<Vector3> vertices =  new List<Vector3>();
+    List<Vector2> uvs = new List<Vector2>();
+    List<int> triangles = new List<int>();
+
+    public BlockType[,,] chunkBlocks = new BlockType[chunkWidth + 2, chunkHeight, chunkWidth + 2];
+
+    public enum FaceDirection {Top, Bottom, Right, Left, Front, Back};
+
+    public static Dictionary<FaceDirection, Vector3[]> FaceVertexMap = new Dictionary<FaceDirection, Vector3[]>()
     {
-        // Fast Noise
-        FastNoise noise = new FastNoise();
+       {FaceDirection.Top, new Vector3[]{new Vector3(0,1,0), new Vector3(0,1,1), new Vector3(1,1,1), new Vector3(1,1,0)}},
+        //010 011 111 110
+       {FaceDirection.Bottom, new Vector3[]{new Vector3(0,0,1), new Vector3(0,0,0), new Vector3(1,0,0), new Vector3(1,0,1)}},
+        //001 000 100 101
+       {FaceDirection.Right, new Vector3[]{new Vector3(1,0,0), new Vector3(1,1,0), new Vector3(1,1,1), new Vector3(1,0,1)}},
+        //100 110 111 101
+       {FaceDirection.Left, new Vector3[]{new Vector3(0,0,1), new Vector3(0,1,1), new Vector3(0,1,0), new Vector3(0,0,0)}},
+        //001 011 010 000
+       {FaceDirection.Front, new Vector3[]{new Vector3(1,0,1), new Vector3(1,1,1), new Vector3(0,1,1), new Vector3(0,0,1)}},
+        //101 111 011 001
+       {FaceDirection.Back,  new Vector3[]{new Vector3(0,0,0), new Vector3(0,1,0), new Vector3(1,1,0), new Vector3(1,0,0)}}
+    };
 
-        List<Vector3> vertices =  new List<Vector3>();
-        List<int> triangles = new List<int>();
-        List<Vector2> uvs = new List<Vector2>();
-        
-        // Mesh
-        Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+    public void Init(Vector2Int chunkCoord, WorldGenerator world)
+    {
+        this.chunkCoord = chunkCoord;
+        this.world = world;
+    }
 
-        // World
-        BlockType[,,] world = new BlockType[chunkWidth + 2, chunkHeight, chunkWidth + 2];
+    public void populateChunk()
+    {   
+        int xOffset = chunkCoord.x * chunkWidth;
+        int zOffset = chunkCoord.y * chunkWidth;
 
-        // Populate world array
         for(int x = 1; x < chunkWidth + 1; x++)
-            for(int z = 1; z < chunkWidth + 1; z++)
-            {
-                for(int y = 0; y < chunkHeight; y++)
-                {
-                    // float simplex1 = noise.GetSimplex(x * noiseScale, z * noiseScale) * noiseAmplitude;
-                    // float simplex2 = noise.GetSimplex(x * 3f, z * 3f) * 10 * (noise.GetSimplex(x*.3f, z*.3f)+.5f);
-                    // float noiseValue = simplex1 + simplex2 + 5;
+        for(int z = 1; z < chunkWidth + 1; z++)
+        for(int y = 0; y < chunkHeight; y++)
+        {
+            int xGlobal = x + xOffset;
+            int zGlobal = z + zOffset;
+            // float simplex1 = noise.GetSimplex(x * noiseScale, z * noiseScale) * noiseAmplitude;
+            // float simplex2 = noise.GetSimplex(x * 3f, z * 3f) * 10 * (noise.GetSimplex(x*.3f, z*.3f)+.5f);
+            // float noiseValue = simplex1 + simplex2 + 5;
 
-                    // Normalized noise
-                    float n = (noise.GetSimplex(x * noiseScale, z * noiseScale) + 1f) * 0.5f; // de [-1,1] → [0,1]
-                    float noiseValue = n * (chunkHeight - 1);
-                    int surfaceY = Mathf.FloorToInt(noiseValue);
-                    
-                    if (y <= surfaceY - 4)
-                        world[x, y, z] = BlockType.Stone;
-                    else if(y < surfaceY)
-                        world[x, y, z] = BlockType.Dirt;
-                    else if(y == surfaceY) // this is because y == noiseValue is never true due to types. When noise == 22.8 and y 22 it fails
-                        world[x, y, z] = BlockType.Grass;
-                    else
-                        world[x, y, z] = BlockType.Air;
-                }
-            }
+            // Normalized noise
+            float n = (noise.GetSimplex(xGlobal * noiseScale, zGlobal * noiseScale) + 1f) * 0.5f; // de [-1,1] → [0,1]
+            float noiseValue = n * (chunkHeight - 1);
+            int surfaceY = Mathf.FloorToInt(noiseValue);
+            
+            if (y <= surfaceY - 4)
+                chunkBlocks[x, y, z] = BlockType.Stone;
+            else if(y < surfaceY)
+                chunkBlocks[x, y, z] = BlockType.Dirt;
+            else if(y == surfaceY) // this is because y == noiseValue is never true due to int vs float. When noise == 22.8 and y 22 it fails
+                chunkBlocks[x, y, z] = BlockType.Grass;
+            else
+                chunkBlocks[x, y, z] = BlockType.Air;
+        }
+    }
 
-        // BuildMesh: Generate Vertices and Triangles
+    public bool checkNeighbourAir(Vector2Int coords, int x, int y , int z)
+    {
+        if(world.chunks.TryGetValue(coords, out TerrainChunk neighbour))
+        if(neighbour.chunkBlocks[x,y,z] == BlockType.Air)
+            return true;
+
+        return false;
+    }   
+
+    public void AddFace(FaceDirection face, Vector3Int basePos, Vector2Int chunkCoord, BlockType currentBlockType)
+    {
+        int xOffset = chunkCoord.x * chunkWidth;
+        int zOffset = chunkCoord.y * chunkWidth; 
+
+        int currentIndex = vertices.Count;
+
+        foreach (Vector3 vertexPosition in FaceVertexMap[face])
+            vertices.Add(basePos + vertexPosition + new Vector3(xOffset, 0, zOffset));
+
+        // Tris clockwise
+        triangles.Add(currentIndex); // v0
+        triangles.Add(currentIndex + 1); // v1
+        triangles.Add(currentIndex + 2); // v2
+        triangles.Add(currentIndex); // v0
+        triangles.Add(currentIndex + 2); // v2
+        triangles.Add(currentIndex + 3); // v3
+
+        // UVs
+        Block currentBlock = Block.blockData[currentBlockType];
+        Vector2[] faceUV = currentBlock.topUV.GetUVs();
+        uvs.AddRange(faceUV);
+    }
+
+    public void buildMesh()
+    {
+        Mesh mesh = new Mesh();
+
+        // Neighbour Chunks coords
+        Vector2Int eastNeighbourCoords = new Vector2Int(chunkCoord.x + 1, chunkCoord.y);
+        Vector2Int westNeighbourCoords = new Vector2Int(chunkCoord.x - 1, chunkCoord.y);
+        Vector2Int northNeighbourCoords= new Vector2Int(chunkCoord.x, chunkCoord.y + 1);
+        Vector2Int southNeighbourCoords= new Vector2Int(chunkCoord.x, chunkCoord.y - 1);
+
+        // Create Vertices, UVs & Triangles
         for(int x = 1; x < chunkWidth + 1; x++)
         for(int z = 1; z < chunkWidth + 1; z++)
         for(int y = 0; y < chunkHeight; y++)
         {   
-            BlockType currentType = world[x, y, z];
+
+            BlockType currentType = chunkBlocks[x, y, z];
             if(currentType != BlockType.Air)
             {
                 // Upper face (normal facing +y)
-                if(y == chunkHeight - 1 || world[x, y + 1, z] == BlockType.Air)
+                if(y == chunkHeight - 1 || chunkBlocks[x, y + 1, z] == BlockType.Air)
                 {
-                    int i = vertices.Count; 
-
-                    // Add ver
-                    vertices.Add(new Vector3(x, y + 1, z)); // 010 v0
-                    vertices.Add(new Vector3(x, y + 1, z + 1)); // 011 v1
-                    vertices.Add(new Vector3(x + 1, y + 1, z + 1)); // 111 v2
-                    vertices.Add(new Vector3(x + 1, y + 1, z)); // 110 v3
-
-                    // Tris clockwise
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 1); // v1
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i + 3); // v3
-
-                    // UVs
-                    Block currentBlock = Block.blockData[currentType];
-                    Vector2[] faceUV = currentBlock.topUV.GetUVs();
-                    uvs.AddRange(faceUV);
+                    Vector3Int pos = new Vector3Int(x, y, z);
+                    FaceDirection face = FaceDirection.Top;
+                    AddFace(face, pos, chunkCoord, currentType);
                 }
+
                 // Bottom Face
-                if(y > 0 && world[x, y - 1, z] == BlockType.Air)
+                if(y > 0 && chunkBlocks[x, y - 1, z] == BlockType.Air)
                 {
-                    int i = vertices.Count; 
-
-                    // Add ver
-                    vertices.Add(new Vector3(x, y, z + 1)); // 001 v0
-                    vertices.Add(new Vector3(x, y, z)); // 000 v1
-                    vertices.Add(new Vector3(x + 1, y, z)); // 100 v2
-                    vertices.Add(new Vector3(x + 1, y, z + 1)); // 101 v3
-
-                    // Tris
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 1); // v1
-                    triangles.Add(i + 2); // v2
-
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i + 3); // v3
-
-                    // UVs
-                    Block currentBlock = Block.blockData[currentType];
-                    Vector2[] faceUV = currentBlock.bottomUV.GetUVs();
-                    uvs.AddRange(faceUV);
+                    Vector3Int pos = new Vector3Int(x, y, z);
+                    FaceDirection face = FaceDirection.Bottom;
+                    AddFace(face, pos, chunkCoord, currentType);
                 }
 
-                // Right Face (normal facing +x)
-                if (world[x + 1, y, z] == BlockType.Air)
+                // Right Face x +
+                // if(x < chunkWidth || checkNeighbourAir(eastNeighbourCoords,0,y,z))
+                if(chunkBlocks[x + 1, y, z] == BlockType.Air)
                 {
-                    int i = vertices.Count; 
-
-                    vertices.Add(new Vector3(x + 1, y, z)); // 100
-                    vertices.Add(new Vector3(x + 1, y + 1, z)); // 110
-                    vertices.Add(new Vector3(x + 1, y + 1, z + 1)); // 111
-                    vertices.Add(new Vector3(x + 1, y, z + 1)); // 101
-
-                    // Tris clockwise
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 1); // v1
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i + 3); // v3
-
-                    // UVs
-                    Block currentBlock = Block.blockData[currentType];
-                    Vector2[] faceUV = currentBlock.sideUV.GetUVs();
-                    uvs.AddRange(faceUV);
+                    Vector3Int pos = new Vector3Int(x, y, z);
+                    FaceDirection face = FaceDirection.Right;
+                    AddFace(face, pos, chunkCoord, currentType);
                 }
                 // Left Face (normal facing -x)
-                if (world[x - 1, y, z] == BlockType.Air)
+                if (chunkBlocks[x - 1, y, z] == BlockType.Air)
                 {
-                    int i = vertices.Count; 
-
-                    vertices.Add(new Vector3(x, y, z + 1)); // 001   v0
-                    vertices.Add(new Vector3(x, y + 1, z + 1)); // 011   v1
-                    vertices.Add(new Vector3(x, y + 1, z)); // 010  v2
-                    vertices.Add(new Vector3(x, y, z)); // 000  v3
-
-                    // First triangle v0 v3 v2
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 1); // v1
-                    triangles.Add(i + 2); // v2
-                    
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i + 3); // v3 
-
-                    // UVs
-                    Block currentBlock = Block.blockData[currentType];
-                    Vector2[] faceUV = currentBlock.sideUV.GetUVs();
-                    uvs.AddRange(faceUV);
+                    Vector3Int pos = new Vector3Int(x, y, z);
+                    FaceDirection face = FaceDirection.Left;
+                    AddFace(face, pos, chunkCoord, currentType);
                 }
 
                 // Back face z-
-                if (world[x, y, z - 1] == BlockType.Air)
+                if (chunkBlocks[x, y, z - 1] == BlockType.Air)
                 {
-                    int i = vertices.Count; 
-
-                    vertices.Add(new Vector3(x, y, z)); // 000 v0
-                    vertices.Add(new Vector3(x, y + 1, z)); // 010 v0
-                    vertices.Add(new Vector3(x + 1, y + 1, z)); // 110 v0
-                    vertices.Add(new Vector3(x + 1, y, z)); // 100 v0
-                    // Tris
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 1); // v1
-                    triangles.Add(i + 2); // v2
-
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i + 3); // v3 
-
-                    // UVs
-                    Block currentBlock = Block.blockData[currentType];
-                    Vector2[] faceUV = currentBlock.sideUV.GetUVs();
-                    uvs.AddRange(faceUV);
-
+                    Vector3Int pos = new Vector3Int(x, y, z);
+                    FaceDirection face = FaceDirection.Back;
+                    AddFace(face, pos, chunkCoord, currentType);
                 }
+
                 // Front face z+
-                if(world[x, y, z + 1] == BlockType.Air)
+                if(chunkBlocks[x, y, z + 1] == BlockType.Air)
                 {
-                    int i = vertices.Count; 
-
-                    vertices.Add(new Vector3(x + 1, y, z + 1)); // 101
-                    vertices.Add(new Vector3(x + 1, y + 1, z + 1)); // 111
-                    vertices.Add(new Vector3(x, y + 1, z + 1)); // 011
-                    vertices.Add(new Vector3(x, y, z + 1)); // 001
-
-                    // Tris
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 1); // v1
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i); // v0
-                    triangles.Add(i + 2); // v2
-                    triangles.Add(i + 3); // v3 
-
-                    // UVs
-                    Block currentBlock = Block.blockData[currentType];
-                    Vector2[] faceUV = currentBlock.sideUV.GetUVs();
-                    uvs.AddRange(faceUV);
+                    Vector3Int pos = new Vector3Int(x, y, z);
+                    FaceDirection face = FaceDirection.Front;
+                    AddFace(face, pos, chunkCoord, currentType);
                 }
             }
         }
@@ -223,9 +192,9 @@ public class TerrainChunk : MonoBehaviour
 
         mesh.RecalculateNormals();
         mesh.RecalculateTangents();
-
         mesh.RecalculateBounds();
+
         GetComponent<MeshCollider>().sharedMesh = mesh;
-        Debug.Log($"Vertices: {vertices.Count}, Triangles: {triangles.Count / 3}");
+        GetComponent<MeshFilter>().mesh = mesh;
     }
 }
--- Assets/Scripts/Terrain/WorldGenerator.cs
+++ Assets/Scripts/Terrain/WorldGenerator.cs
@@ -1,15 +1,42 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class WorldGenerator : MonoBehaviour
 {
+    public TerrainChunk chunkPrefab;
+
+    private int chunkRadius = 5;
+
+    public Vector2Int chunkCoord = new Vector2Int(0,0);
+
+    public Dictionary<Vector2Int, TerrainChunk> chunks = new Dictionary<Vector2Int, TerrainChunk>();
 
     void Start()
     {
-
+        generateWorld();
     }
 
     void Update()
     {
         
+    }
+
+    public void generateWorld()
+    {   
+        for(int x = -chunkRadius; x <= chunkRadius; x++)
+        for(int z = -chunkRadius; z <= chunkRadius; z++)
+        {   
+            chunkCoord = new Vector2Int(x,z);
+
+            TerrainChunk newChunk = Instantiate(chunkPrefab);
+            newChunk.name = $"Chunk_{x}_{z}";
+
+            chunks.Add(chunkCoord,newChunk);
+
+            newChunk.Init(chunkCoord, this);
+            newChunk.populateChunk();
+            newChunk.buildMesh();
+
+        }
     }
 }
