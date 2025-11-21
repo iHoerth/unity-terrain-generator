c3f3e6eec074cfb4604f1c8526931025e1de1e58
Ignacio Hoerth <hoerth.ignacio@gmail.com>
2025-11-21 17:10:39 -0300

    Inventory Action 'Inventory' added to the input manager to allow inventory to be opened with Tab

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -7,18 +7,15 @@ public class InventoryManager : MonoBehaviour
 {
     public GameObject invGroup;
     public Image crosshair;
+    public Button darkBgToggle;
     public Button invBtn;
 
     public bool inventoryOpened = false;
 
-    // void Awake()
-    // {
-
-    // }
-
-    void Update()
+    void Awake()
     {
-
+        invBtn.onClick.AddListener(ToggleInventory);
+        darkBgToggle.onClick.AddListener(ToggleInventory);
     }
 
     public void ToggleInventory()
@@ -29,6 +26,4 @@ public class InventoryManager : MonoBehaviour
         crosshair.gameObject.SetActive(!inventoryOpened);
         invBtn.gameObject.SetActive(!inventoryOpened);
     }   
-
-    
 }
